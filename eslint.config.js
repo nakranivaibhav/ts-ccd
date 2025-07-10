0a8c05c35398ad36ffbd67b0f575786b330c8108
nakranivaibhav <nakranivaibhav14@gmail.com>
2025-07-10 11:56:37 +0530

    groot

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist/**", "coverage/**", "node_modules/**"]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+];
